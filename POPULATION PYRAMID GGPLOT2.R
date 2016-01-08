25bbe8ac00152ffb6ba746de290d794e61866599
Juan Galeano <jgaleano@ced.uab.es>
2016-01-08 11:58:16 +0100

    Update POPULATION PYRAMID GGPLOT2.R

--- POPULATION PYRAMID GGPLOT2.R
+++ POPULATION PYRAMID GGPLOT2.R
@@ -75,52 +75,61 @@ load(url('http://gedemced.uab.cat/images/POP_ENC_AMB_2014GRUPS.Rdata')) #read da
 # African-born (5), Asian-born (6) and other (0)
 
 COMPOSED_PYRAMID <- ggplot(POP_ENC_AMB_2014GRUPS, aes(x=edad, y=n, fill=sexo))+
-                    geom_bar(subset=.(sexo=="Mujeres\nEspaña"|sexo=="Mujeres\nLatinoamerica"|
-                                      sexo=="Mujeres\nEuropa_Occ."|sexo=="Mujeres\nEuropa_Or."|
-                                      sexo=="Mujeres\nÁfrica"|sexo=="Mujeres\nAsia"|
-                                      sexo=="Mujeres\nOtros"),colour="black", stat="identity")+
-                    geom_bar(subset=.(sexo=="Hombres\nEspaña"|sexo=="Hombres\nLatinoamerica"|
-                                      sexo=="Hombres\nEuropa_Occ."|sexo=="Hombres\nEuropa_Or."|
-                                      sexo=="Hombres\nÁfrica"|sexo=="Hombres\nAsia"|
-                                      sexo=="Hombres\nOtros"),colour="black", stat="identity")+
-                    coord_flip()+
-                    scale_y_continuous(limits=c(-750,750),
-                                       breaks = c(-750, -500, -250,0,250,500,750), 
-                                       labels = paste0(as.character(c(seq(750, 0, -250), seq(250, 750, 250))), "")) + 
-                    scale_x_continuous(breaks=seq(0,110,5)) +
-                    scale_fill_manual(values = c("#00FF00","#FE2EC8","#F4FA58","#0000FF",
-                                                 "#2ECCFA","#FF0000","#A4A4A4",
-                                                 "#00FF00","#FE2EC8","#F4FA58","#0000FF",
-                                                 "#2ECCFA","#FF0000","#A4A4A4"))+
-                    # we genereate and ad hoc legend
-                    annotate("text", x = 107.5, y = -120, label = "Hombres",size=13)+
-                    annotate("text", x = 107.5, y = 108, label = "Mujeres",size=13)+
-                    annotate("rect", xmin = 106, xmax = 109, ymin = 400, ymax =450, alpha=1, fill="#F4FA58")+
-                    annotate("text", x = 107.5, y = 526, label = "España",size=11)+
-                    annotate("rect", xmin = 96, xmax = 99, ymin = 400, ymax =450, alpha=1, fill="#FF0000")+
-                    annotate("text", x = 97.5, y = 583, label = "Latinoamérica",size=11)+
-                    annotate("rect", xmin = 86, xmax = 89, ymin = 400, ymax =450, alpha=1, fill="#0000FF")+
-                    annotate("text", x = 87.5, y = 625, label = "Europa Occidental",size=11)+
-                    annotate("rect", xmin = 76, xmax = 79, ymin = 400, ymax =450, alpha=1, fill="#2ECCFA")+
-                    annotate("text", x = 77.5, y = 600, label = "Europa Oriental",size=11)+
-                    annotate("rect", xmin = 66, xmax = 69, ymin = 400, ymax =450, alpha=1, fill="#00FF00")+
-                    annotate("text", x = 67.5, y = 515, label = "África",size=11)+
-                    annotate("rect", xmin = 56, xmax = 59, ymin = 400, ymax =450, alpha=1, fill="#FE2EC8")+
-                    annotate("text", x = 57.5, y = 500, label = "Asia",size=11)+
-                    #
-                    theme(plot.title = element_text(lineheight=1.6, size=20, face="bold"),
-                    legend.title = element_blank(),
-                    legend.text = element_text(colour="black", size = 35),
-                    legend.position="none",
-                    legend.background = element_rect(fill="#FFFFFF"),
-                    axis.title.x = element_text(angle = 0,vjust=0.5, size=30,colour="black"),
-                    axis.text.x  = element_text(angle = 0,vjust=0.5, size=35,colour="black"),
-                    axis.title.y = element_text(angle = 90,vjust=0.5, size=30,colour="black"),
+  geom_bar(data = POP_ENC_AMB_2014GRUPS[(POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nEspaña"|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nLatinoamerica"|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nEuropa_Occ."|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nEuropa_Or."|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nÁfrica"|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nAsia"|
+                                         POP_ENC_AMB_2014GRUPS$sexo=="Mujeres\nOtros"),], 
+           colour = I("Black"),stat="identity", size=.3, colour="black",
+           aes(x = edad, y = n,fill = sexo), alpha = 1)+
+  geom_bar(data = POP_ENC_AMB_2014GRUPS[(POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nEspaña"|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nLatinoamerica"|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nEuropa_Occ."|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nEuropa_Or."|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nÁfrica"|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nAsia"|
+                                           POP_ENC_AMB_2014GRUPS$sexo=="Hombres\nOtros"),], 
+           colour = I("Black"),stat="identity", size=.3, colour="black",
+           aes(x = edad, y = n,fill = sexo), alpha = 1)+
+  coord_flip()+
+  scale_y_continuous(limits=c(-750,750),
+                     breaks = c(-750, -500, -250,0,250,500,750), 
+                     labels = paste0(as.character(c(seq(750, 0, -250), seq(250, 750, 250))), "")) + 
+  scale_x_continuous(breaks=seq(0,110,5)) +
+  scale_fill_manual(values = c("#00FF00","#FE2EC8","#F4FA58","#0000FF",
+                               "#2ECCFA","#FF0000","#A4A4A4",
+                               "#00FF00","#FE2EC8","#F4FA58","#0000FF",
+                               "#2ECCFA","#FF0000","#A4A4A4"))+
+  # we genereate and ad hoc legend
+  annotate("text", x = 107.5, y = -120, label = "Hombres",size=13)+
+  annotate("text", x = 107.5, y = 108, label = "Mujeres",size=13)+
+  annotate("rect", xmin = 106, xmax = 109, ymin = 400, ymax =450, alpha=1, fill="#F4FA58")+
+  annotate("text", x = 107.5, y = 526, label = "España",size=11)+
+  annotate("rect", xmin = 96, xmax = 99, ymin = 400, ymax =450, alpha=1, fill="#FF0000")+
+  annotate("text", x = 97.5, y = 583, label = "Latinoamérica",size=11)+
+  annotate("rect", xmin = 86, xmax = 89, ymin = 400, ymax =450, alpha=1, fill="#0000FF")+
+  annotate("text", x = 87.5, y = 625, label = "Europa Occidental",size=11)+
+  annotate("rect", xmin = 76, xmax = 79, ymin = 400, ymax =450, alpha=1, fill="#2ECCFA")+
+  annotate("text", x = 77.5, y = 600, label = "Europa Oriental",size=11)+
+  annotate("rect", xmin = 66, xmax = 69, ymin = 400, ymax =450, alpha=1, fill="#00FF00")+
+  annotate("text", x = 67.5, y = 515, label = "África",size=11)+
+  annotate("rect", xmin = 56, xmax = 59, ymin = 400, ymax =450, alpha=1, fill="#FE2EC8")+
+  annotate("text", x = 57.5, y = 500, label = "Asia",size=11)+
+  #
+  theme(plot.title = element_text(lineheight=1.6, size=20, face="bold"),
+        legend.title = element_blank(),
+        legend.text = element_text(colour="black", size = 35),
+        legend.position="none",
+        legend.background = element_rect(fill="#FFFFFF"),
+        axis.title.x = element_text(angle = 0,vjust=0.5, size=30,colour="black"),
+        axis.text.x  = element_text(angle = 0,vjust=0.5, size=35,colour="black"),
+        axis.title.y = element_text(angle = 90,vjust=0.5, size=30,colour="black"),
         axis.text.y  = element_text( vjust=0.5, size=35,colour="black"),
         plot.background = element_rect(fill = "#FFFFFF"))+ylab("")+xlab("Edad")
 
 plot(COMPOSED_PYRAMID)
-
 # we save the plot as a png file using the function ggsave
 
 setwd("C:/Users/ced/Desktop/")
